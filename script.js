57340d13377a2b2795b474c1f82e4cca0335aae8
Noha Salah <101585161+NohaEltohamy@users.noreply.github.com>
2025-05-27 12:01:03 +0300

    Update script.js from remote repo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("This is a script file loaded from the 'script.js' file.");
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
