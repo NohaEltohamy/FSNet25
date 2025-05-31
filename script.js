9d512f908bc9e574b10785e0287f2f7e8128e5f2
Noha Salah <nohasalah@iti.gov.eg>
2025-05-31 09:40:41 +0300

    extra work to my main branch from frontEnd new branch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("This is a script file loaded from the 'script.js' file.");
+console.log("This is a script file loaded from'script.js' file.");
 function validateForm() {
   let x = document.forms["myForm"]["fname"].value;
   if (x == "") {
